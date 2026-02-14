575f9733b9e1d182fc9a4428cb6786367aae3ff2
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-14 22:23:00 +0200

    Auto update 2026-02-14 22:23

--- resources/views/dashboard/contractors/index.blade.php
+++ resources/views/dashboard/contractors/index.blade.php
@@ -283,7 +283,7 @@
             <!-- Modal madameenName-->
             <div class="modal modal-lg rtl" id="mota3ahdeenDataModern" tabindex="-1" aria-labelledby="exampleModalLabel"
                 aria-hidden="true">
-                <div class="modal-dialog modal-dialog-scrollable">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h1 class="modal-title fs-5" id="exampleModalLabel">
