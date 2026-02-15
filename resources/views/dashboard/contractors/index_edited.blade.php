367becdde6a1266d21a24d6e94a3b572199d0f58
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-15 10:01:32 +0200

    Auto update 2026-02-15 10:01

--- resources/views/dashboard/contractors/index_edited.blade.php
+++ resources/views/dashboard/contractors/index_edited.blade.php
@@ -283,7 +283,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h1 class="modal-title fs-5" id="exampleModalLabel">
-                                بيانات المتعهد
+                               Z بيانات المتعهد
                             </h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                 aria-label="Close">X</button>
