0aa18b0526e5969759ca22276460aad5989e2eb5
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-17 10:50:35 +0200

    Auto update 2026-02-17 10:50

--- resources/views/public/candidates/profile.blade.php
+++ resources/views/public/candidates/profile.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $candidate->user?->name ?? 'المرشح' }}</title>
     <link rel="stylesheet" href="{{ asset('assets/site/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/site/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/site/css/all.min.css') }}">
 </head>
 <body>
 <div class="candidate-public-page" dir="rtl">
