de8d90eb35a7ed48c23fff41650479d23bd48892
mando2411 <10312023101163@stud.cu.edu.eg>
2026-03-29 04:18:30 +0200

    Auto update 2026-03-29 04:18

--- resources/views/dashboard/sorting/index.blade.php
+++ resources/views/dashboard/sorting/index.blade.php
@@ -432,7 +432,9 @@
   }
 
   .paper-report-modal-close {
+    --bs-btn-close-bg: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23102a43'%3e%3cpath d='M.293.293a1 1 0 011.414 0L8 6.586 14.293.293a1 1 0 111.414 1.414L9.414 8l6.293 6.293a1 1 0 01-1.414 1.414L8 9.414l-6.293 6.293A1 1 0 01.293 14.293L6.586 8 .293 1.707a1 1 0 010-1.414z'/%3e%3c/svg%3e");
     opacity: 1;
+    filter: none;
     background-color: #ffffff;
     border: 1px solid #cbd9e8;
     border-radius: 10px;
